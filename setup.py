336328c8792739c7b39a37e5d1784564ef6fce4b
tacocatCLAUS <116687416+tacocatCLAUS@users.noreply.github.com>
2025-09-04 19:33:14 -0400

    setup.py

--- setup.py
+++ setup.py
@@ -32,16 +32,55 @@ def create_and_install():
         sys.exit(1)
 
 if __name__ == "__main__":
+print("Its...")
 print("                ██████╗ ██╗███████╗███╗   ███╗ ██████╗ ")
 print(" ʕ 0 ᴥ 0ʔ      ██╔════╝ ██║╚══███╔╝████╗ ████║██╔═══██╗")
 print(" |      |      ██║  ███╗██║  ███╔╝ ██╔████╔██║██║   ██║")
 print(" |      |      ██║   ██║██║ ███╔╝  ██║╚██╔╝██║██║   ██║")
 print(" |      |      ╚██████╔╝██║███████╗██║ ╚═╝ ██║╚██████╔╝")
 print("+--------+      ╚═════╝ ╚═╝╚══════╝╚═╝     ╚═╝ ╚═════╝ ")
-    create_and_install()
-    print("\nPIP package install complete. To activate the virtual environment, run:")
-    if sys.platform == "win32":
-        print(f"  .\\{VENV_NAME}\\Scripts\\activate")
+print("Welcome to the gizmo installer! We're going to get you set up in a jiffy.")
+print("To run this program, please confirm that you have a capable system, you have the option to run the script using an api, but if you want to use either the voice, ollama, or the rag you will NEED a capable system.")
+print("I have a 4070 super and it gets very slow if ollama and tts are running.")
+print("Can your system handle large ai workloads? If you say no RAG, ollama, and voice will be disable by default. Y/N")
+while True:
+canithandleit = input()
+    if canithandleit == "Y":
+        print("Got it. What model do you want to by default?")
+        print("1. OpenAI")
+        print("2. Hack Club (NO PERSONAL USE)")
+        print("3. Ollama")
+        break
+    elif canithandleit == "N":
+        print("Got it. What model do you want to by default?")
+        print("1. OpenAI")
+        print("2. Hack Club (NO PERSONAL USE)")
+        print("3. Ollama (NOT RECCOMENDED)")
+        break
+else:
+    print("Input not Y/N, try again.")
+
+while True:
+model = input("Which one? (1-3):")
+    if model == 1:
+        model = "openai"
+        print("What is your OpenAI key?")
+        openai_api_key = input("It is:")
+        break
+    elif model == 2:
+        model = "hc"
+        break
+    elif model == 3:
+        model = "ollama"
+        break
     else:
-        print(f"  source ./{VENV_NAME}/bin/activate")
-    print("")
+        print("Please enter a number between 1 & 3.")
+
+
+
+create_and_install()
+print("\nPIP package install complete. To if you need to activate the virtual environment, run:")
+if sys.platform == "win32":
+    print(f"  .\\{VENV_NAME}\\Scripts\\activate")
+else:
+    print(f"  source ./{VENV_NAME}/bin/activate")
