16ac0c533dc2811c58ed5d9a4a77f9aceef7adbe
iamaschool <26bear@wtisbury.org>
2025-09-17 12:54:00 +0000

    got rid of all secrets in V0.3

--- public releases/V.03/bom.py
+++
@@ -1,8 +0,0 @@
-from termcolor import colored, cprint
-string = "Hello World"
-x = 12
-goofy = True
-
-print("\033[92mHello \033[91mWorld\033[0m")
-if goofy == True:
-    cprint("boom " * x, 'magenta')
